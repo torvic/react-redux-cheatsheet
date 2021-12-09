01947ee1127a8d5ec0a474871d205a0935d015a3
torvic <130111@unsaac.edu.pe>
2021-12-08 19:07:27 -0500

    Feat: react redux toolkit basic

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,34 @@
+import { useDispatch } from 'react-redux'
+import { authActions } from '../../store/auth.slice'
+import styles from './index.module.css'
+
+const Header = ({ isAuth }) => {
+  const dispatch = useDispatch()
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+  }
+
+  return (
+    <header className={styles.header}>
+      <h1>Redux Auth</h1>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/UserProfile/index.jsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css';
+
+const UserProfile = () => {
+  return (
+    <main className={styles.profile}>
+      <h2>My User Profile</h2>
+    </main>
+  );
+};
+
+export default UserProfile;
